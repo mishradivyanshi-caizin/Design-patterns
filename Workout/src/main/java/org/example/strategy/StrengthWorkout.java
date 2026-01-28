9aa9cfdea53e4a2633d8abb7f9a4ec3d1bf6c9fa
mishradivyanshi-caizin <mishra.divaynshi@caizin.com>
2026-01-28 11:51:35 +0530

    implemented workout strategy pattern

---
+++ Workout/src/main/java/org/example/strategy/StrengthWorkout.java
@@ -0,0 +1,8 @@
+package org.example.strategy;
+
+public class StrengthWorkout implements Workout {
+
+    public void performWorkout() {
+        System.out.println("Performing Strength Training: Weights and Resistance");
+    }
+}
---
+++ Workout/src/main/java/org/example/strategy/YogaWorkout.java
@@ -0,0 +1,8 @@
+package org.example.strategy;
+
+public class YogaWorkout implements Workout {
+
+    public void performWorkout() {
+        System.out.println("Performing Yoga: Stretching and Breathing");
+    }
+}
