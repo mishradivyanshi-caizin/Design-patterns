c403c76f0dd6a3bacf8c0d4430754445cf62d2d2
mishradivyanshi-caizin <mishra.divaynshi@caizin.com>
2026-01-28 10:36:03 +0530

    adding duck fly interface

---
+++ DuckProblem/src/main/java/org/example/Duck.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public abstract class Duck {
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+
+    public abstract void display();
+}
