f6c551d6192fae91c10b220b2ec904ddf6e84a4e
mishradivyanshi-caizin <mishra.divaynshi@caizin.com>
2026-01-28 10:51:12 +0530

    added quake behaviours

---
+++ DuckProblem/src/main/java/org/example/behaviours/QuackBehavior.java
@@ -0,0 +1,5 @@
+package org.example.behaviours;
+
+public interface QuackBehavior {
+    void quack();
+}
---
+++ DuckProblem/src/main/java/org/example/behaviours/Squeak.java
@@ -0,0 +1,7 @@
+package org.example.behaviours;
+
+public class Squeak implements QuackBehavior {
+    public void quack() {
+        System.out.println("Squeak!");
+    }
+}
